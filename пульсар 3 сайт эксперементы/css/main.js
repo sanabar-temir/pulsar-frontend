da75e59cdd1fa9fd4c08c500f5d5ab4a71ae5fc5
sanabar-temir <125647677+sanabar-temir@users.noreply.github.com>
2026-01-13 14:51:47 +0600

    Add files via upload

---
+++ пульсар 3 сайт эксперементы/css/main.js
@@ -0,0 +1,27 @@
+function toggleTheme() {
+  document.body.classList.toggle("light");
+}
+
+new Swiper(".newsSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+});
+
+
+let slides = document.querySelectorAll('.slide');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 3500);
